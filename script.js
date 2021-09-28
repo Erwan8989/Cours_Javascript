0efab3cd1f189fd998f230db5be4304000fa8a1f
Erwan8989 <77726753+Erwan8989@users.noreply.github.com>
2021-09-28 17:02:05 +0200

    Exercice PS5 (terminé)

---
+++ script.js
@@ -0,0 +1,30 @@
+// ************************* Début cours *************************
+
+// let prenom = prompt("Saisissez votre prénom")
+// let nom = "Doe"
+// let age = 10
+// let professeur = true
+// let eleves = ["Alban", "Meymoun", "San"]
+
+// if (age < 18) {
+//     alert("Tu es mineur et tu t'appelles " + prenom)
+// }
+// else{
+//     alert("Tu as plus de 25 ans")
+// }
+
+
+// ************************* Exercice PS5 *************************
+
+prix = prompt("Saisissez le prix de votre PS5")
+
+if (prix <= 350) {
+    alert("Fonce alphonse !")
+}
+else if (prix <= 500) {
+    alert("Il s'agit du prix de base")
+}
+else {
+    alert("C'est quoi encore cette arnaque !")
+}
+
