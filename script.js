b4ff890efc305e8d2c9a1dbe2ff44ffb3546d59f
Noodles-boop <72260596+Noodles-boop@users.noreply.github.com>
2021-09-28 16:54:37 +0200

    ..

---
+++ script.js
@@ -0,0 +1,37 @@
+alert("Hello World")
+
+
+// let prenom = "John"
+// let nom = "Doe"
+// let age = 43
+// let personnagePrincipal = true
+// let qualites = ["courageux", "vaillant", "intelligent"]
+
+// let age = prompt("Quel est ton âge ?")
+
+// if (age >= 18) {
+//   alert ("tu es majeur")
+// }
+// else if (age < 18 && age >= 15) {
+//   alert ("tu es adolescent")
+// }
+// else {
+//   alert ("tu es enfant")
+// }
+
+let prix = prompt("Quel est le prix de la PS5 ?")
+
+if (prix <= 500) {
+  alert ("Cest une excellente affaire ")
+}
+else if (prix >= 600) {
+  alert ("C'est une arnaque !")
+}
+else  {
+  alert ("C'est une bonne affaire :)")
+}
+
+
+// let age = prompt("Saisissez votre âge")
+// alert ("vous avez " + age + " ans")
+
