1db10099b862427ec6541088808adce426af9192
Erwan8989 <77726753+Erwan8989@users.noreply.github.com>
2021-09-28 18:01:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// ************************* Début cours *************************
+// // ************************* Début cours *************************
 
 // let prenom = prompt("Saisissez votre prénom")
 // let nom = "Doe"
@@ -14,17 +14,54 @@
 // }
 
 
-// ************************* Exercice PS5 *************************
+// // ************************* Exercice PS5 *************************
 
-prix = prompt("Saisissez le prix de votre PS5")
+// prix = prompt("Saisissez le prix de votre PS5")
 
-if (prix <= 350) {
-    alert("Fonce alphonse !")
-}
-else if (prix <= 500) {
-    alert("Il s'agit du prix de base")
-}
-else {
-    alert("C'est quoi encore cette arnaque !")
+// if (prix <= 350) {
+//     alert("Fonce alphonse !")
+// }
+// else if (prix <= 500) {
+//     alert("Il s'agit du prix de base")
+// }
+// else {
+//     alert("C'est quoi encore cette arnaque !")
+// }
+
+
+// // ************************* Exercice plus ou moins *************************
+
+// let age = 50
+// let age_entree = 0
+// let coup = 1
+
+// alert("Devinez le chiffre entre 0 et 100")
+    
+// while (age != age_entree) {
+//     age_entree = prompt("Entrez une valeur")
+
+//     if (age == age_entree) {
+//         alert("Tu as trouvé en " + coup + " coup(s)")
+//     }
+//     else if (age < age_entree) {
+//         alert("C'est moins")
+//         coup++
+//     }
+//     else{
+//         alert("C'est plus")
+//         coup++
+//     }
+// }
+
+
+// ************************* Exercice Fahrenheit en dégré celsius *************************
+
+function convertir(nombre1){
+    let result = (nombre1-32)*5/9
+
+    return result
 }
 
+let degré = prompt("Entrez une valeur en Fahrenheit, elle sera convertit en dégré celsius")
+
+alert (convertir(degré))
