c670137d898d9b4e2f8f487f63afe70fa3598502
Noodles-boop <72260596+Noodles-boop@users.noreply.github.com>
2021-10-19 09:25:10 +0200

    Update Pokedex.js

--- Pokedex/Pokedex.js
+++ Pokedex/Pokedex.js
@@ -1,5 +1,3 @@
-
-
 let monBouton = document.querySelector(".monBouton")
 let nom_input = document.querySelector(".nom")
 let type_input = document.querySelector(".type")
@@ -38,7 +36,7 @@ function AddElement() {
 function Refreshpokemon() {
     list.innerHTML = ""
     for (let i = 0; i < pokemon.length; i++) {
-        let nomPokemon = document.createElement("div")  
+        let nomPokemon = document.createElement("div")
         nomPokemon.innerHTML = pokemon[i].nom
         list.appendChild(nomPokemon)
 
@@ -59,3 +57,50 @@ function RemoveElement(index) {
     pokemon.splice(index, 1)
     Refreshpokemon()
 }
+
+
+
+// const container_pokemon = document.getElementById('container_pokemon');
+// const pokemon_number = 150;
+
+// // Construction d'une boucle for id 1 à 150
+
+// const fetchPokemons = async () => {
+//     for (let i = 1; 1 <= pokemon_number; i++) {
+//         await getPokemon(i);
+//     }
+// };
+
+// const getPokemon = async id => {
+//     const url = 'https://pokeapi.co/api/v2/pokemon/${id}';
+//     const res = await fetch(url);
+//     const pokemon = await res.json();
+//     createPokemonCard(pokemon);
+// }
+
+// const createPokemonCard = (pokemon) => {
+//     const pokemonEl = document.createElement('div');
+//     pokemonEl.classList.add('pokemon');
+//     const {
+//         id,
+//         name,
+//         sprites,
+//         types
+//     } = pokemon;
+//     const type = types[0].type.name;
+
+//     const pokeInnerHTML = `<div class="img-container">
+//     <img src="${sprites.front_default}" alt="${name}" />
+//     </div>
+//     <div class="info">
+//     <span class="number"> ${id}</span>
+//     <h3 class="name">${name}</h3>
+//     <small class="type">Type: <span>${type}</span>
+//     </small>
+//     </div>  
+//     `;
+//     pokemonEl.InnerHTML = pokeInnerHTML;
+//     poke_container.appendChild(pokemonEl);
+// }
+
+// fetchPokemons();
