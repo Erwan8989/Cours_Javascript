2b76528c9ac0ff7fc9ba1fbf006b8e950d966475
Noodles-boop <Oldoni.jeremy@outlook.fr>
2022-02-26 10:14:00 +0100

    ...

--- Pokedex/Pokedex.js
+++ Pokedex/Pokedex.js
@@ -1,48 +1,48 @@
-let monBouton = document.querySelector(".monBouton");
-let nom_input = document.querySelector(".nom");
-let type_input = document.querySelector(".type");
-let url_input = document.querySelector(".url");
-let info = document.querySelector(".info");
+let nom = document.querySelector(".nomPoke");
+let type = document.querySelector(".typePoke");
+let url = document.querySelector(".imgPoke");
+let bouton = document.querySelector(".bouton");
+let carte = document.querySelector(".content_container");
 
-let pokemon = [];
+let pokedex = [];
 
-let saved_txt = window.localStorage.getItem("pokemon_saved");
-if (saved_txt) {
-  let saved_obj = JSON.parse(saved_txt);
-  pokemon = saved_obj;
-  Refreshpokemon();
+let save = window.localStorage.getItem("pokedex_save");
+if (save) {
+  let save_obj = JSON.parse(save);
+  pokedex = save_obj;
+  RefreshPokedex();
 }
 
-monBouton.addEventListener("click", function () {
-  AddElement();
+bouton.addEventListener("click", function () {
+  add();
 });
 
-function AddElement() {
+function add() {
   let infos = {
-    nom: nom_input.value,
-    type: type_input.value,
-    url: url_input.value,
+    nom: nom.value,
+    type: type.value,
+    url: url.value,
   };
-  pokemon.push(infos);
+  pokedex.push(infos);
 
-  nom_input.value = "";
-  type_input.value = "";
-  url_input.value = "";
+  nom.value = "";
+  type.value = "";
+  url.value = "";
 
-  Refreshpokemon();
+  RefreshPokedex();
 }
 
-function Refreshpokemon() {
-  info.innerHTML = "";
-  for (let i = 0; i < pokemon.length; i++) {
+function RefreshPokedex() {
+  carte.innerHTML = "";
+  for (let i = 0; i < pokedex.length; i++) {
     let div = document.createElement("div");
-    info.appendChild(div);
+    carte.appendChild(div);
 
     let bloc = document.createElement("div");
-    bloc.innerHTML = pokemon[i].nom;
+    bloc.innerHTML = pokedex[i].nom;
     div.appendChild(bloc);
 
-    var val = pokemon[i].url,
+    var val = pokedex[i].url,
       src = val,
       img = document.createElement("img");
 
@@ -50,8 +50,12 @@ function Refreshpokemon() {
     div.appendChild(img);
 
     let bloc3 = document.createElement("div");
-    bloc3.innerHTML = pokemon[i].type;
+    bloc3.innerHTML = pokedex[i].type;
     div.appendChild(bloc3);
+
+    // var img = document.createElement("img");
+    // img.src = "test.png";
+    // div.appendChild(img);
 
     let delete_btn = document.createElement("button");
     delete_btn.className = "delete_btn";
@@ -61,16 +65,78 @@ function Refreshpokemon() {
       RemoveElement(i);
     });
 
-    // if (pokemon.length == 0) {
-    //     carte.className = "info"
+    // if (pokedex[i].type == "Feu" || pokedex[i].type == "feu") {
+    //   div.className = "carte bloc feu col-6";
+    //   carte.className = "row";
+    // } else if (pokedex[i].type == "Plante" || pokedex[i].type == "plante") {
+    //   div.className = "carte bloc plante col-6";
+    //   carte.className = "row";
+    // } else if (pokedex[i].type == "Acier" || pokedex[i].type == "acier") {
+    //   div.className = "carte bloc acier col-6";
+    //   carte.className = "row";
+    // } else if (pokedex[i].type == "Combat" || pokedex[i].type == "combat") {
+    //   div.className = "carte bloc combat col-6";
+    //   carte.className = "row";
+    // } else if (pokedex[i].type == "Dragon" || pokedex[i].type == "dragon") {
+    //   div.className = "carte bloc dragon col-6";
+    //   carte.className = "row";
+    // } else if (pokedex[i].type == "Eau" || pokedex[i].type == "eau") {
+    //   div.className = "carte bloc eau col-6";
+    //   carte.className = "row";
+    // } else if (
+    //   pokedex[i].type == "Electrique" ||
+    //   pokedex[i].type == "electrique"
+    // ) {
+    //   div.className = "carte bloc electrique col-6";
+    //   carte.className = "row";
+    // } else if (pokedex[i].type == "Fée" || pokedex[i].type == "fée") {
+    //   div.className = "carte bloc fee col-6";
+    //   carte.className = "row";
+    // } else if (pokedex[i].type == "Glace" || pokedex[i].type == "glace") {
+    //   div.className = "carte bloc glace col-6";
+    //   carte.className = "row";
+    // } else if (pokedex[i].type == "Insecte" || pokedex[i].type == "insecte") {
+    //   div.className = "carte bloc insecte col-6";
+    //   carte.className = "row";
+    // } else if (pokedex[i].type == "Poison" || pokedex[i].type == "poison") {
+    //   div.className = "carte bloc poison col-6";
+    //   carte.className = "row";
+    // } else if (pokedex[i].type == "Normal" || pokedex[i].type == "normal") {
+    //   div.className = "carte bloc normal col-6";
+    //   carte.className = "row";
+    // } else if (pokedex[i].type == "Psy" || pokedex[i].type == "psy") {
+    //   div.className = "carte bloc psy col-6";
+    //   carte.className = "row";
+    // } else if (pokedex[i].type == "Roche" || pokedex[i].type == "roche") {
+    //   div.className = "carte bloc roche col-6";
+    //   carte.className = "row";
+    // } else if (pokedex[i].type == "Sol" || pokedex[i].type == "sol") {
+    //   div.className = "carte bloc sol col-6";
+    //   carte.className = "row";
+    // } else if (pokedex[i].type == "Spectre" || pokedex[i].type == "spectre") {
+    //   div.className = "carte bloc spectre col-6";
+    //   carte.className = "row";
+    // } else if (pokedex[i].type == "Tenebre" || pokedex[i].type == "tenebre") {
+    //   div.className = "carte bloc tenebre col-6";
+    //   carte.className = "row";
+    // } else if (pokedex[i].type == "Vol" || pokedex[i].type == "vol") {
+    //   div.className = "carte bloc vol col-6";
+    //   carte.className = "row";
+    // } else {
+    //   div.className = "carte bloc white col-6";
+    //   carte.className = "row";
     // }
-
-    let texte = JSON.stringify(pokemon);
-    window.localStorage.setItem("pokemon_saved", texte);
   }
+
+  if (pokedex.length == 0) {
+    carte.className = "carte";
+  }
+
+  let texte = JSON.stringify(pokedex);
+  window.localStorage.setItem("pokedex_save", texte);
 }
 
 function RemoveElement(index) {
-  pokemon.splice(index, 1);
+  pokedex.splice(index, 1);
   RefreshPokedex();
 }
